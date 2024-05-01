baca67856ff50c896df29b79f2536d1ef73be12e
rohan821101 <79596636+rohan821101@users.noreply.github.com>
2024-05-02 00:27:15 +0530

    Update update_app.py

--- update_app.py
+++ update_app.py
@@ -7,12 +7,12 @@ model = pickle.load(open('model_SVM.pkl', 'rb'))
 st.title('Fake News AI')
 input_news = st.text_area('Enter the News')
 
-if st.button('Predict'):
+if st.button('Submit'):
     # 2. predict
     result = model.predict([input_news])[0]
     
     # 3. Display
     if result == 1:
-        st.header("True")
+        st.header("It is a genuine news")
     else:
-        st.header("Fake")
+        st.header("It is a fake news")
